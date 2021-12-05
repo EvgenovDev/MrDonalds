dbe845979a34224b38d64b400e327a75ce4d4640
Roman Evgenov <Roman Evgenov>
2021-12-05 18:04:15 +0400

    Сверстана шапка

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import Header from "./Components/Header";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle `
+	html {
+		box-sizing: border-box;
+	}
+
+	*,
+	::before,
+	::after {
+		box-sizing: inherit;
+	}
+
+	body {
+		font-size: 20px;
+		color: black;
+		background-color: #f0f0f0;
+		font-family: Roboto, sans-serif;
+		margin: 0px;
+		padding: 0px;
+	}
+
+	h1,h2,h3 {
+		padding: 0px;
+		margin: 0px;
+		font-family: Pacifico;
+	}
+
+	img {
+		max-width: 100%;
+		heigth: auto;
+	}
+`
+function App() {
+  return (
+    <div className="App">
+		 <GlobalStyle/>
+		 <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,76 @@
+import  styled  from "styled-components";
+import logo from "../images/logo.svg";
+import user from "../images/user.svg"
+
+const NavBar = styled.header `
+	width: 100%;
+	background-color: #299B01;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 15px;
+`
+
+const Logo = styled.div `
+	display: flex;
+	align-items: center;
+	cursor: pointer;
+`
+
+const NavTitle = styled.h1 `
+	font-size: 24px;
+	color: white;
+	margin-left: 12px;
+`
+
+const User = styled.div `
+	display: flex;
+	flex-direction: column;
+	cursor: pointer;
+	align-items: center;
+`
+
+const LogoImg = styled.img `
+	width: 50px;
+`
+
+const UserLogo = styled.img ` 
+	width: 32px;
+	margin-bottom: 5px;
+`
+
+const UserText = styled.span `
+	color: white;
+	font-size: 16px;
+	::after {
+		content: '';
+		background: white;
+		height: 1px;
+		width: 100%;
+		display: block;
+		opacity: 0;
+	}
+	:hover{
+		::after {
+			opacity: 1;
+		}
+	}
+`
+
+const Header = (props) => {
+	return (
+		<NavBar>
+			<Logo>
+				<LogoImg src={logo} alt="Логотип"/>
+				<NavTitle >MRDonald’s</NavTitle>
+			</Logo>
+			<User>
+				<UserLogo src={user} alt="Пользователь"/>
+				<UserText>Войти</UserText>
+			</User>
+
+		</NavBar>
+	)
+} 
+
+export default Header;
